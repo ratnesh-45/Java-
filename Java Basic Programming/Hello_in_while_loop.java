c439ffaf33d0956a02cf7ba12587dd46c7a67c11
Ratnesh Kumar <154334703+ratnesh-45@users.noreply.github.com>
2024-06-20 14:26:05 +0530

    Add files via upload

---
+++ Java Basic Programming/Hello_in_while_loop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Hello_in_while_loop {
+  public static void main(String args[]) {
+    int counter = 0;
+    while (counter < 10) {
+      System.out.println("Hello coder!");
+      counter++;
+    }
+  }
+}
