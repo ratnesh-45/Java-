7857df7a8599f3a070c7a675ce61519952a111d0
Ratnesh Kumar <154334703+ratnesh-45@users.noreply.github.com>
2024-06-20 14:39:19 +0530

    Add files via upload

---
+++ 3. Condition/Switch_statement.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Switch_statement {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    // Switch Statement:-
+    System.out.print("Enter the Number:-");
+    int number = sc.nextInt();
+
+    switch (number) {
+      case 1:
+        System.out.println("Samosa");
+        break;
+      case 2:
+        System.out.println("Burger");
+        break;
+      case 3:
+        System.out.println("Mango shake");
+        break;
+
+      default:
+        System.out.println("Kuchh Nahi milega");
+        break;
+    }
+  }
+}
