89c4570891de6d93c3a5efd2d7ccfe426a1e9d1b
Ratnesh Kumar <154334703+ratnesh-45@users.noreply.github.com>
2024-06-20 14:26:28 +0530

    Add files via upload

---
+++ Java Basic Programming/if_else_statement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class if_else_statement {
+  public static void main(String arg[]) {
+    // Define the if-else statement:-
+    int age = 22;
+    if (age >= 18) {
+      System.out.println("You can eligible for vote");
+    } else {
+      System.out.println("Not adult");
+    }
+  }
+}
