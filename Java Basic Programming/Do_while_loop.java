21447e37dd5d28b943493a87f2938a02dbd12d9b
Ratnesh Kumar <154334703+ratnesh-45@users.noreply.github.com>
2024-06-20 14:24:39 +0530

    Add files via upload

---
+++ Java Basic Programming/Do_while_loop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Do_while_loop {
+  public static void main(String args[]) {
+    // print something using do-while loop:-
+    int counter = 1;
+    do {
+      System.out.println(counter);
+      counter++;
+    } while (counter <= 10);
+
+  }
+}
